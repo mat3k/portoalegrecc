d5f61600952c274f724a08ced3faa5f9a9405a23
Mateusz Baltyn <mateusz.baltyn@gmail.com>
2014-12-18 12:53:31 +0100

    Konfiguracja mapy

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -90,9 +90,9 @@ var Map = Class.extend({
     	}	
 	},
 	createMap: function() {
-		var startLat = $.cookie("map.lat") != null ? $.cookie("map.lat") : 50.060;
-		var startLng = $.cookie("map.lng") != null ? $.cookie("map.lng") : 19.959;
-		var startZoom = $.cookie("map.zoom") != null ? $.cookie("map.zoom") : 13;
+		var startLat = $.cookie("map.lat") != null ? $.cookie("map.lat") : 51.500;
+		var startLng = $.cookie("map.lng") != null ? $.cookie("map.lng") : 19.000;
+		var startZoom = $.cookie("map.zoom") != null ? $.cookie("map.zoom") : 7;
 		startZoom = parseInt(startZoom);
 		
 		this.portoalegreLocation = new google.maps.LatLng(startLat, startLng);
@@ -200,25 +200,124 @@ var Map = Class.extend({
 	},
 	
 	getStyle: function() {
-		return [{
-			featureType: "all",
-			elementType: "all",
-			stylers:  [
-				{ invert_lightness: true },
-				{ visibility: "on" },
-				{ saturation: 10 },
-				{ lightness: 30 },
-				{ gamma: 0.5 },
-				{ hue: "#435158" }]
-		    },
-			{
-			featureType: "water",
-			elementType: "all",
-			stylers: [
-				{ hue: "#00AAFF" },
-				{ lightness: 30 },
-				{ saturation: 20 }]
-		}];
+		return [
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 65
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 51
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 30
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 40
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.province",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -100
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#ffff00"
+            },
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -97
+            }
+        ]
+    }
+];
 	},
 	
 	getMap: function() {
